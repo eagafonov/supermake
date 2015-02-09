7418cbd0062c094cda97f25191523654b06af3d5
Eugene Agafonov <e.a.agafonov@gmail.com>
2015-02-09 17:35:37 +0300

    [DJANGO] RUNSERVER_HTTP_BIND is used to run dev server

--- python-django.mk
+++ python-django.mk
@@ -1,7 +1,9 @@
 smf-check::
 
+RUNSERVER_HTTP_BIND?='127.0.0.1:8000'
+
 runserver:
-	$(SANDBOX) $(PYTHON) manage.py runserver
+	$(SANDBOX) $(PYTHON) manage.py runserver $(RUNSERVER_HTTP_BIND)
 
 collectstatic:
 	$(SANDBOX) ./manage.py collectstatic --noinput --clear
