3c88a1616efc32a8391664ff31eb7a1b15967ba1
Eugene Agafonov <e.a.agafonov@gmail.com>
2017-03-25 13:05:38 +0300

    [tools] Protect supermake source repo from nested bootstraping

--- tools/supermake-bootstrap.sh
+++ tools/supermake-bootstrap.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
-set -x 
+set -x
 set -e
+
+test $(dirname $(readlink -fe $0)) = $(pwd) && (echo "E: do not run supermake-bootstrap in supermake"; exit 1)
 
 test ! -d supermake && git submodule add https://github.com/eagafonov/supermake.git supermake
 touch requirements-dev.txt
