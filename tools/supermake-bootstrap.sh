776bdfab7695ed7d7344ede5db2acb4842f084eb
Eugene Agafonov <e.a.agafonov@gmail.com>
2017-06-17 21:17:28 +0300

    Use Makefile template for bootsraping

--- tools/supermake-bootstrap.sh
+++ tools/supermake-bootstrap.sh
@@ -3,11 +3,15 @@
 set -x
 set -e
 
-test $(dirname $(readlink -fe $0)) = $(pwd) && (echo "E: do not run supermake-bootstrap in supermake"; exit 1)
+SM_DIR=$(dirname $(readlink -fe $0))
+
+test $SM_DIR = $(pwd) && (echo "E: do not run supermake-bootstrap in supermake"; exit 1)
+
+test ! -d .git && git init
 
 test ! -d supermake && git submodule add https://github.com/eagafonov/supermake.git supermake
-touch requirements-dev.txt
-touch requirements-test.txt
+test ! -f requirements-dev.txt && touch requirements-dev.txt && git add requirements-dev.txt
+test ! -f requirements-test.txt && touch requirements-test.txt && git add requirements-test.txt
+test ! -f .gitignore && (echo sandbox >> .gitignore && git add .gitignore) || (echo I: .gitignore exists)
 
-test ! -f Makefile && echo "include supermake/python-sandbox.mk" > Makefile
-
+test ! -f Makefile && cp $SM_DIR/Makefile.template Makefile && git add Makefile
